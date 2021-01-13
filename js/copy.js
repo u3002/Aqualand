715082617d0df33897b38258102fb3273a90c0c5
u3002 <20873340+u3002@users.noreply.github.com>
2021-01-13 19:42:42 +0800

    .

--- js/copy.js
+++ js/copy.js
@@ -1,9 +1,9 @@
-const btn = document.querySelector('#btn');
+ï»¿const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
 	const input = document.querySelector('#demoInput');
 	input.select();
 	if (document.execCommand('copy')) {
 		document.execCommand('copy');
-		console.log('¸´ÖÆ³É¹¦');
+		console.log('è¤‡è£½æˆåŠŸ');
 	}
 })
